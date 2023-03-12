68d9d385cef7a46016b42fa7fe08779c3148805d
dkapoor75 <115779058+dkapoor75@users.noreply.github.com>
2023-03-12 14:47:42 +1100

    Final Upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,153 @@
-// Get the Roadster endpoint
-// const sample_data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+// Use D3 fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
 
-// // Fetch the JSON data and console log it
-// d3.json(sample_data).then(function(data) {
-//   console.log(data);
-// });
+let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+d3.json(url).then((importedData) => {
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data);
+	console.log(importedData);
+
+	let data = importedData;
+
+	// Dynamically add test Subject ID No. to the dropdown menus
+	let names = data.names;
+
+	names.forEach((name) => {
+		d3.select("#selDataset").append("option").text(name);
+	})
+
+	// Initializes the page with default plots
+	function init() {
+
+		// Choose data for test ID No. 940 plotted as default
+
+		defaultDataset = data.samples[0];
+        console.log(defaultDataset);
+
+		// Select all sample_values, otu_ids and otu_labels of the selected test ID
+		allSampleValuesDefault = defaultDataset.sample_values;
+		allOtuIdsDefault = defaultDataset.otu_ids;
+		allOtuLabelsDefault = defaultDataset.otu_labels;
+
+		// Select the top 10 OTUs for the ID with their sample_values, otu_ids and otu_labels
+		sampleValuesDefault = allSampleValuesDefault.slice(0, 10).reverse();
+		otuIdsDefault = allOtuIdsDefault.slice(0, 10).reverse();
+		otuLabelsDefault = allOtuLabelsDefault.slice(0, 10).reverse();
+
+		console.log(sampleValuesDefault);
+		console.log(otuIdsDefault);
+		console.log(otuLabelsDefault);
+
+		// BAR CHART
+		// Add trace for the default Data
+		let trace1 = {
+			x: sampleValuesDefault,
+			y: otuIdsDefault.map(outId => `OTU ${outId}`),
+			text: otuLabelsDefault,
+			type: "bar",
+			orientation: "h"
+		};
+
+		// data
+		let barData = [trace1];
+
+		// Apply the group bar mode to the layout
+		let barlayout = {
+			title: `<b>Bar Chart<b>`,
+			xaxis: { title: "Sample Value"},
+			yaxis: { title: "OTU ID"},
+			autosize: false,
+			width: 450,
+			height: 600
+		}
+
+		// Render the plot to the div tag with id "bar"
+		Plotly.newPlot("bar", barData, barlayout);
+
+		// BUBBLE CHART
+		let trace2 = {
+			x: allOtuIdsDefault,
+			y: allSampleValuesDefault,
+			text: allOtuLabelsDefault,
+			mode: 'markers',
+			marker: {
+				color: allOtuIdsDefault,
+				size: allSampleValuesDefault
+			}
+		};
+		
+		let bubbleData = [trace2];
+		
+		let bubbleLayout = {
+			title: '<b>Bubble Chart<b>',
+			xaxis: { title: "OTU ID"},
+			yaxis: { title: "Sample Value"}, 
+			showlegend: false,
+		};
+		
+		Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+		// DEMOGRAPHIC INFO
+		demoDefault = data.metadata.filter(sample => sample.id === 940)[0];
+		console.log(demoDefault);
+
+		// Display each key-value pair from the metadata JSON object
+		Object.entries(demoDefault).forEach(
+			([key, value]) => d3.select("#sample-metadata").append("p").text(`${key.toUpperCase()}: ${value}`));
+
+	}
+
+	init();
+
+
+	// Call updateBar() when a change takes place to the DOM
+	d3.selectAll("#selDataset").on("change", updatePlot);
+
+	// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+	// This function is called when a dropdown menu item is selected
+	function updatePlot() {
+
+		// Use D3 to select the dropdown menu
+			let inputElement = d3.select("#selDataset");
+
+		// Assign the value of the dropdown menu option to a variable
+			let inputValue = inputElement.property("value");
+			console.log(inputValue);
+
+		// Filter the dataset based on inputValue ID
+			dataset = data.samples.filter(sample => sample.id === inputValue)[0];
+			console.log(dataset);
+
+		// Select all sample_values, otu_ids and otu_labels of the selected test ID
+			allSampleValues = dataset.sample_values;
+			allOtuIds = dataset.otu_ids;
+			allOtuLabels = dataset.otu_labels;
+
+		// Select the top 10 OTUs for the ID with their sample_values, otu_ids and otu_labels
+		top10Values = allSampleValues.slice(0, 10).reverse();
+		top10Ids = allOtuIds.slice(0, 10).reverse();
+		top10Labels = allOtuLabels.slice(0, 10).reverse();
+
+		// BAR CHART
+		Plotly.restyle("bar", "x", [top10Values]);
+		Plotly.restyle("bar", "y", [top10Ids.map(outId => `OTU ${outId}`)]);
+		Plotly.restyle("bar", "text", [top10Labels]);
+
+		// BUBBLE CHART
+		Plotly.restyle('bubble', "x", [allOtuIds]);
+		Plotly.restyle('bubble', "y", [allSampleValues]);
+		Plotly.restyle('bubble', "text", [allOtuLabels]);
+		Plotly.restyle('bubble', "marker.color", [allOtuIds]);
+		Plotly.restyle('bubble', "marker.size", [allSampleValues]);
+
+		// DEMOGRAPHIC INFO
+		metainfo = data.metadata.filter(sample => sample.id == inputValue)[0];
+
+		// Clear out current contents in the panel
+		d3.select("#sample-metadata").html("");
+
+		// Display each key-value pair from the metadata JSON object
+		Object.entries(metainfo).forEach(([key, value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`));
+	}
 });
